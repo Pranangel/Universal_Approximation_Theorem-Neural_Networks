f803b8c1ce33fb6f4077d50607e42a8efc6562ff
Pranangel <121978243+Pranangel@users.noreply.github.com>
2026-05-18 15:33:15 -0700

    Refactor ANN.py for improved structure and readability
    -Rewrote Layer.__init__ to use ActivationFunction types (activations.py) instead of a string key to a dictionary.
    -Rewrote BasicANN.train() and basicANN.backPropagation() to use ErrorFunction types (errors.py) instead of a string key to a dictionary.
    -Wrapped function and derivative for errors and activations into classes that extend ActivationFunction and ErrorFunction.

--- ANN.py
+++ ANN.py
@@ -1,128 +1,8 @@
 #Author: Pranangel
 #Purpose: Making the building blocks for a customizable artificial neural network.
 
-#https://medium.com/analytics-vidhya/activation-functions-optimization-techniques-and-loss-functions-75a0eea0bc31
 import numpy as np
 from numpy import ndarray
-
-class Activations: #TODO
-    def __init__(self): pass
-
-class Losses: #TODO
-    def __init__(self) -> None:
-        pass
-
-def sigmoid(z: ndarray) -> ndarray:
-    """Takes a matrix as an argument and applies the sigmoid function to every value in the matrix.
-    
-    Algebraically, sigmoid is defined as 1 / (1 + e^-z). However, this implementation uses
-    (1 / 1 + exp(-z)) for z > 0 and exp(z) / (1 + exp(z)) for z < 0, avoiding overflow errors.
-
-    This method creates two masks of the inputted matrix, one for values > 0 and another for values <= 0.
-    The optimized version of the sigmoid is applied to a result matrix with respect to the masks.
-    
-    (Source: https://blog.dailydoseofds.com/p/sigmoid-and-softmax-are-not-implemented)
-    """
-
-    result = np.zeros_like(a=z, dtype=float)
-    #Boolean matrices contain 0s (false) or 1s (true).
-    positiveMask = z > 0  #Mask for values > 0
-    negativeMask = z <= 0 #Mask for values <= 0
-
-    #The masks are being used inside brackets to have the function apply only to values with true.
-    a = np.exp(-1 * z[positiveMask])
-    result[positiveMask] = 1 / (1 + a)
-
-    a = np.exp(z[negativeMask])
-    result[negativeMask] = a / (a + 1)
-    
-    return result
-
-def derivSigmoid(z: ndarray) -> ndarray:
-    a = sigmoid(z)
-    return a * (1 - a)
-
-def relu(x: ndarray) -> ndarray:
-    return np.maximum(0, x)
-
-def derivRelu(x: ndarray) -> ndarray:
-    positiveMask = x > 0
-    result = np.zeros_like(a=x, shape=x.shape)
-    result[positiveMask] = 1
-
-    return result
-
-#FIXME
-# def softmax(mat: ndarray) -> ndarray:
-#     """Takes a matrix as an argument and calculates a probability distribution 
-#     represented by a matrix with the same shape."""
-
-#     sumExp = np.sum(np.exp(mat))
-#     return np.exp(mat) / sumExp
-
-ACTIVATION_FUNCS = {
-    "sigmoid" : sigmoid,
-    "relu"    : relu,
-    #"softmax" : softmax
-}
-
-DERIV_ACTIVATION_FUNCS = {
-    "sigmoid_d" : derivSigmoid,
-    "relu_d"    : derivRelu,
-}
-
-#TODO: log cosh loss
-
-def squaredError(predicted: ndarray, actual: ndarray) -> ndarray:
-    return (predicted - actual) ** 2
-
-def derivSquaredError(predicted: ndarray, actual: ndarray) -> ndarray:
-    return 2 * (predicted - actual)
-
-#TODO: add per sample (axis=1), per output (axis=0)
-def meanSquaredError(predicted: ndarray, actual: ndarray): #outputs floating[Any]
-    return np.mean((predicted - actual) ** 2)
-
-def derivMeanSquaredError(predicted: ndarray, actual: ndarray) -> ndarray:
-    return 2 * (predicted - actual) / predicted.size
-
-#FIXME
-# def meanAbsError(predicted: ndarray, actual: ndarray) -> ndarray:
-#     m1, n1 = predicted.shape #TODO: check for same-size shape
-#     m2, n2 = actual.shape
-
-#     return np.abs(predicted - actual)  / n1
-
-# def derivMeanAbsError(predicted: ndarray, actual: ndarray) -> ndarray:
-#     m1, n1 = predicted.shape #TODO: check for same-size shape
-#     m2, n2 = actual.shape
-
-#     diff = predicted - actual
-#     copy = np.ones((m1, m2))
-
-#     if (diff > 0):
-#         return copy * (1 / n1)
-#     elif (diff < 0):
-#         return copy * (-1 / n1)
-#     return copy * 0
-
-#TODO: Add binary cross entropy
-ERROR_FUNCS = {
-    "SE"  : squaredError,
-    "MSE" : meanSquaredError,
-    # "MAE" : meanAbsError
-}
-
-DERIV_ERROR_FUNCS = {
-    "SE_d"  : derivSquaredError,
-    "MSE_d" : derivMeanSquaredError,
-    # "MAE_d" : derivMeanAbsError
-}
-
-class Activation: 
-
-    def __init__(self):
-        pass
 
 class Loss: pass
 
@@ -132,37 +12,36 @@ class Trainer: pass
 
 class Tester: pass
 
+from activations import *
+from errors import *
 class BasicANN:
     """
     BasicANN initializes a predefined artificial neural network. This model expects (x,y) pairs
     (matrix of shape n, 2) and produces a z-value for each pair (matrix of shape n, 1).
     
     Default architecture:
-    1st layer: 1 neuron with sigmoid activation
+    1st layer: 1 neuron with ReLU activation
     2nd/output layer: 1 neuron with sigmoid activation
     Loss function: Mean Squared Error
     """
     
-    #TODO: If the programmer wants to build their own, they must pass ALL OF numLayers, numNeurons, AND activations.
-    def __init__(self, numFeatures: int, batchSize=None, numLayers=2, numNeurons=[1, 1], activations=["sigmoid", "sigmoid"]) -> None:
-        if (batchSize == None): batchSize = 32 #FIXME
+    #FIXME: If the programmer wants to build their own, they must pass ALL OF numLayers, numNeurons, AND activations.
+    def __init__(self, numFeatures: int, batchSize: int, numLayers: int, numNeurons: list[int], activations:list[ActivationFunction]) -> None:
+        if (batchSize   == None): batchSize = 32
+        if (numLayers   == None): numLayers = 2
+        #TODO: how should this behave if there is a discrepancy between list sizes, or if only one list is inputted?
+        if (numNeurons  == None): numNeurons = [1,1]
+        if (activations == None): activations=[ActivationFunction.RELU, ActivationFunction.SIGMOID]
         
         self.features  = numFeatures
         self.batchSize = batchSize
         self.input: ndarray
         self.error: ndarray
 
-        #TODO: store in dictionary?
         self.layers       = []
-        self.weights      = []
-        self.biases       = []
-        self.pOutputs     = []
-        self.aOutputs     = []
-        self.activations  = []
-
         self.addLayers(numLayers, numNeurons, activations)
 
-    def addLayers(self, numLayers: int, neuronsPerLayer: list[int], activationsPerLayer: list[str]) -> None:
+    def addLayers(self, numLayers: int, neuronsPerLayer: list[int], activationsPerLayer: list[ActivationFunction]) -> None:
         if (numLayers == len(neuronsPerLayer)) and (numLayers == len(activationsPerLayer)):
             m, n = 0, 0
             for l in range(numLayers):
@@ -172,7 +51,7 @@ class BasicANN:
                 else:
                     m, n = self.layers[l - 1].getAOutputs().shape
 
-                self.layers.append(Layer(inputM=m, inputN=n, neurons=neuronsPerLayer[l], funcName=activationsPerLayer[l]))
+                self.layers.append(Layer(inputM=m, inputN=n, neurons=neuronsPerLayer[l], activation=activationsPerLayer[l]))
 
     #TODO
     # def clear(): pass
@@ -195,27 +74,26 @@ class BasicANN:
     
     #TODO:
     #   -enforce proper dimensionality of z argument
-    #   -Check valid lossFunc string
-    def backPropagation(self, input: ndarray, z: ndarray, learnRate: float, lossFuncName: str):
-        self.__backPropagation(input, z, learnRate, lossFuncName)
+    def backPropagation(self, input: ndarray, z: ndarray, learnRate: float, errorFunc: ErrorFunction):
+        self.__backPropagation(input, z, learnRate, errorFunc)
 
-    #TODO
-    def __backPropagation(self, input: ndarray, z: ndarray, learnRate: float, lossFuncName: str):
+    def __backPropagation(self, input: ndarray, z: ndarray, learnRate: float, errorFunc: ErrorFunction):
         """Uses a rolling variable to track gradients. Every current layer updates the gradient,
         and the layers higher up the chain will reuse this gradient, updating it for each
         pass backwards towards the input layer.
         """
         last = len(self.layers) - 1
         zPredicted = self.layers[last].getAOutputs()
-        self.error = ERROR_FUNCS[lossFuncName](zPredicted, z) #FIXME: should self.error be an instance member at all?
-        dEdZPredicted = DERIV_ERROR_FUNCS[f"{lossFuncName}_d"](zPredicted, z)
+        #FIXME: Some functions may return a float instead of ndarray
+        self.error = errorFunc.getFunc()(zPredicted, z) #FIXME: should self.error be an instance member at all?
+        dEdZPredicted = errorFunc.getDeriv()(zPredicted, z)
         gradient = np.zeros(input.shape)
 
         for i in reversed(range(last)):
             layer = self.layers[i]
 
             p = layer.getPOutputs()
-            actFunc = layer.getActivationDeriv()
+            actFunc = layer.getActivation().getDeriv()
             a = input.T
             if (i != 0):
                 a = self.layers[i - 1].getAOutputs().T #rolling gradient does NOT get updated with a
@@ -247,12 +125,11 @@ class BasicANN:
 
     """
     Batch gradient descent.
-
-    If this was stochastic, it would take the whole training dataset and inside of the epoch loop,
-    there would be another loop that does forward and backward for each point in the dataset
     """
+    # If this was stochastic, it would take the whole training dataset and inside of the epoch loop,
+    # there would be another loop that does forward and backward for each point in the dataset
     #FIXME: safe file writing
-    def train(self, input: ndarray, z: ndarray, learnRate: float, epochs: int, lossFuncName: str, displayOutputs = False, saveFile = ""):
+    def train(self, input: ndarray, z: ndarray, learnRate: float, epochs: int, errorFunc: ErrorFunction, displayOutputs = False, saveFile = ""):
         samples, _ = input.shape
         
         for i in range(epochs):
@@ -260,7 +137,7 @@ class BasicANN:
                 inputBatch = input[j:j+self.batchSize] #TODO: Does numpy fill 0s for <32 features?
                 predBatch  = z[j:j+self.batchSize]
                 a = self.forwardPropagation(inputBatch)
-                self.backPropagation(input=inputBatch, z=predBatch, learnRate=learnRate, lossFuncName=lossFuncName)
+                self.backPropagation(input=inputBatch, z=predBatch, learnRate=learnRate, errorFunc=errorFunc)
                 self.__display(i + 1, round(j / self.batchSize) + 1, a, predBatch)
 
     def test(self, testInput: ndarray, displayPredictions=False, saveFile = "", testOutput = None) -> ndarray:
@@ -288,23 +165,21 @@ class BasicANN:
         
         return a
 
+"""
+A Layer is a matrix with three properties: its dimensions n and m, and an activation function.
+Terminology: the p ('product') matrix is the product between the previous layer and weights, the a
+('activation') matrix is the p matrix that has an activation function applied to it.
+
+A Layer expects the shape of the inputted matrix, the neurons/columns that will be stored,
+and the activation function's name.
+"""
 class Layer:
     index = 0 #Static variable to track the number of layers created, used for debugging purposes.
 
-    """
-    A Layer is a matrix with three properties: its dimensions n and m, and an activation function.
-    Terminology: the p ('product') matrix is the product between the previous layer and weights, the a
-    ('activation') matrix is the p matrix that has an activation function applied to it.
-    
-    A Layer expects the shape of the inputted matrix, the neurons/columns that will be stored,
-    and the activation function's name.
-    """
-    
     #TODO: handle checking for valid shapes
     """inputM and inputN are the dimensions of the inputted matrix, neurons specify the number of columns in the output matrix."""
-    def __init__(self, inputM: int, inputN: int, neurons: int, funcName: str): #TODO: what if funcName is invalid?
-        self.activationFunc  = ACTIVATION_FUNCS[funcName]
-        self.activationDeriv = DERIV_ACTIVATION_FUNCS[f"{funcName}_d"]
+    def __init__(self, inputM: int, inputN: int, neurons: int, activation: ActivationFunction): #TODO: what if funcName is invalid?
+        self.activation      = activation
         
         #TODO: Xavier/Glorot initialization for sigmoid, He initialization for ReLU
         self.weights = np.random.rand(inputN, neurons)
@@ -319,11 +194,11 @@ class Layer:
 
     """Forward propagation algorithm: returns a numpy array of matrix multiplication and an applied activation
     function."""
-    #TODO: implement mini-batching or process per-sample; ensure shapes reflect (batch_size, features).
+    #TODO: ensure shapes reflect (batch_size, features).
     def forward(self, input: ndarray, displayParams = False) -> ndarray:
         #matrix multiplication
         self.p = input @ self.weights + self.biases
-        self.a = self.activationFunc(self.p)
+        self.a = self.activation.getFunc()(self.p)
 
         if (displayParams):
             if self.layerIndex == 0:
@@ -342,11 +217,8 @@ class Layer:
         self.weights -= dw * learnRate
         self.biases -= db * learnRate
 
-    def getActivationFunc(self): #Returns a function wrapper
-        return self.activationFunc
-    
-    def getActivationDeriv(self): #Returns a function wrapper for the derivative of the activation function
-        return self.activationDeriv
+    def getActivation(self) -> ActivationFunction:
+        return self.activation
 
     def getWeights(self) -> ndarray:
         return self.weights
@@ -359,19 +231,19 @@ class Layer:
     
     def getAOutputs(self) -> ndarray:
         return self.a
-
+    
 #Loading data from csv and loading into a numpy matrix
 import pandas as pd
 dataDF = pd.read_csv("training_data.csv")
 dataDF = dataDF.sample(frac=1).reset_index(drop=True)
 
-n = 3200 #Specify the number of rows to extract for training and testing
+n = 10 #Specify the number of rows to extract for training and testing
 trainInputs = dataDF[["x", "y"]].iloc[:n].to_numpy()
 trainOutputs = dataDF[["z"]].iloc[:n].to_numpy()
 
-model1 = BasicANN(numFeatures=2, batchSize=64, numLayers=2, numNeurons=[10, 1], activations=["sigmoid", "relu"])
 #TODO: generate non-normalized data and compare results to normalized data
-model1.train(input=trainInputs, z=trainOutputs, learnRate=0.1, epochs=100, lossFuncName="MSE", displayOutputs=True)
+model1 = BasicANN(numFeatures=2, batchSize=2, numLayers=2, numNeurons=[10, 1], activations=[Sigmoid(), Sigmoid()])
+model1.train(input=trainInputs, z=trainOutputs, learnRate=0.1, epochs=10, errorFunc=MeanSquaredError(), displayOutputs=True)
 
 #TODO: add visualization to training and testing
 # from mpl_toolkits.mplot3d import Axes3D
