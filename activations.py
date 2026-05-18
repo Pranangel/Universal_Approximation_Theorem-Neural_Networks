edf8e1af90cb6d8ab75333b3c33feed7f691922c
Pranangel <121978243+Pranangel@users.noreply.github.com>
2026-05-18 15:25:41 -0700

    Added activations and errors modules

---
+++ activations.py
@@ -0,0 +1,76 @@
+#Author: Pranangel
+#Purpose: This module contains wrapper classes housing activation functions and their methods.
+
+import numpy as np
+from numpy import ndarray
+from Function import Function
+
+#https://medium.com/analytics-vidhya/activation-functions-optimization-techniques-and-loss-functions-75a0eea0bc31
+class ActivationFunction(Function):
+    pass
+
+class Sigmoid(ActivationFunction):
+    @staticmethod
+    def getFunc():
+        return sigmoid
+    
+    @staticmethod
+    def getDeriv():
+        return derivSigmoid
+    
+def sigmoid(z: ndarray) -> ndarray:
+    """Takes a matrix as an argument and applies the sigmoid function to every value in the matrix.
+    
+    Algebraically, sigmoid is defined as 1 / (1 + e^-z). However, this implementation uses
+    (1 / 1 + exp(-z)) for z > 0 and exp(z) / (1 + exp(z)) for z < 0, avoiding overflow errors.
+
+    This method creates two masks of the inputted matrix, one for values > 0 and another for values <= 0.
+    The optimized version of the sigmoid is applied to a result matrix with respect to the masks.
+    
+    (Source: https://blog.dailydoseofds.com/p/sigmoid-and-softmax-are-not-implemented)
+    """
+
+    result = np.zeros_like(a=z, dtype=float)
+    #Boolean matrices contain 0s (false) or 1s (true).
+    positiveMask = z > 0  #Mask for values > 0
+    negativeMask = z <= 0 #Mask for values <= 0
+
+    #The masks are being used inside brackets to have the function apply only to values with true.
+    a = np.exp(-1 * z[positiveMask])
+    result[positiveMask] = 1 / (1 + a)
+
+    a = np.exp(z[negativeMask])
+    result[negativeMask] = a / (a + 1)
+    
+    return result
+
+def derivSigmoid(z: ndarray) -> ndarray:
+    a = sigmoid(z)
+    return a * (1 - a)
+
+class ReLU:
+    @staticmethod
+    def getFunc():
+        return relu
+    
+    @staticmethod
+    def getDeriv():
+        return derivRelu
+    
+def relu(x: ndarray) -> ndarray:
+    return np.maximum(0, x)
+
+def derivRelu(x: ndarray) -> ndarray:
+    positiveMask = x > 0
+    result = np.zeros_like(a=x, shape=x.shape)
+    result[positiveMask] = 1
+
+    return result
+
+#FIXME
+# def softmax(mat: ndarray) -> ndarray:
+#     """Takes a matrix as an argument and calculates a probability distribution 
+#     represented by a matrix with the same shape."""
+
+#     sumExp = np.sum(np.exp(mat))
+#     return np.exp(mat) / sumExp
---
+++ errors.py
@@ -0,0 +1,61 @@
+#Author: Pranangel
+#Purpose: This module contains wrapper classes housing error functions and their methods.
+
+import numpy as np
+from numpy import ndarray
+from Function import Function
+
+class ErrorFunction(Function):
+    pass
+
+class SquaredError(ErrorFunction):
+
+    @staticmethod
+    def squaredError(predicted: ndarray, actual: ndarray) -> ndarray:
+        return (predicted - actual) ** 2
+
+    @staticmethod
+    def derivSquaredError(predicted: ndarray, actual: ndarray) -> ndarray:
+        return 2 * (predicted - actual)
+
+class MeanSquaredError(ErrorFunction):
+    @staticmethod
+    def getFunc():
+        return meanSquaredError
+
+    @staticmethod
+    def getDeriv():
+        return derivMeanSquaredError
+
+#TODO: add per sample (axis=1), per output (axis=0)
+def meanSquaredError(predicted: ndarray, actual: ndarray): #TODO: outputs floating[Any]
+    return np.mean((predicted - actual) ** 2)
+    
+def derivMeanSquaredError(predicted: ndarray, actual: ndarray) -> ndarray:
+    return 2 * (predicted - actual) / predicted.size
+
+#TODO
+# class BinaryCrossEntropy(ErrorFunction):
+#     pass
+
+#FIXME
+# class MeanAbsoluteError(ErrorFunction):
+    # @staticmethod
+    # def meanAbsError(predicted: ndarray, actual: ndarray) -> ndarray:
+    #     m1, n1 = predicted.shape #TODO: check for same-size shape
+    #     m2, n2 = actual.shape
+    #     return np.abs(predicted - actual)  / n1
+
+    # @staticmethod
+    # def derivMeanAbsError(predicted: ndarray, actual: ndarray) -> ndarray:
+    #     m1, n1 = predicted.shape #TODO: check for same-size shape
+    #     m2, n2 = actual.shape
+
+    #     diff = predicted - actual
+    #     copy = np.ones((m1, m2))
+
+    #     if (diff > 0):
+    #         return copy * (1 / n1)
+    #     elif (diff < 0):
+    #         return copy * (-1 / n1)
+    #     return copy * 0
