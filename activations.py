3c967bad63b19d91320a538beb4c5042d287e9eb
Pranangel <121978243+Pranangel@users.noreply.github.com>
2026-06-14 12:36:23 -0700

    Refactor activation functions and add LeakyReLU
    Activations:
        1: No longer extend abstract_function
        2: Have their respective functions and derivatives housed INSIDE the class as static methods
        3: Now return ndarrays instead of callables

--- activations.py
+++ activations.py
@@ -3,103 +3,154 @@
 
 import numpy as np
 from numpy import ndarray
-from abstract_function import Function
 
-#https://medium.com/analytics-vidhya/activation-functions-optimization-techniques-and-loss-functions-75a0eea0bc31
-class ActivationFunction(Function):
+class ActivationFunction():
     """
     Wrapper class for all activations. Currently supports:
 
     
         -Sigmoid
-        -ReLU
+        -ReLU variations
+            -Normal
+            -Leaky
         -Identity
     """
-    pass
+    @staticmethod
+    def getFunc(input: ndarray):
+        return ActivationFunction.__identity(input)
+
+    @staticmethod
+    def getDeriv(input: ndarray):
+        return ActivationFunction.__deriv(input)
+
+    @staticmethod
+    def __identity(x: ndarray) -> ndarray:
+        scalar = 1.
+        return scalar * x
+
+    @staticmethod
+    def __deriv(x: ndarray) -> ndarray:
+        scalar = 1.
+        return scalar * np.ones(x.shape)
 
 class Sigmoid(ActivationFunction):
 
     @staticmethod
-    def getFunc():
-        return sigmoid
+    def getFunc(input: ndarray):
+        return Sigmoid.__sigmoid(input)
     
     @staticmethod
-    def getDeriv():
-        return derivSigmoid
+    def getDeriv(input: ndarray):
+        return Sigmoid.__deriv(input)
     
-    
-def sigmoid(z: ndarray) -> ndarray:
-    """Takes a matrix as an argument and applies the sigmoid function to every value in the matrix.
-    
-    Algebraically, sigmoid is defined as 1 / (1 + exp(-z)). However, this implementation uses
-    (1 / 1 + exp(-z)) for z > 0 and exp(z) / (1 + exp(z)) for z < 0, avoiding overflow errors.
 
-    This method creates two masks of the inputted matrix, one for values > 0 and another for values <= 0.
-    The optimized version of the sigmoid is applied to a result matrix with respect to the masks.
-    
-    (Source: https://blog.dailydoseofds.com/p/sigmoid-and-softmax-are-not-implemented)
-    """
+    @staticmethod
+    def __sigmoid(x: ndarray) -> ndarray:
+        """Takes a matrix as an argument and applies the sigmoid function to every value in the matrix.
+        
+        Algebraically, sigmoid is defined as 1 / (1 + exp(-z)). However, this implementation uses
+        (1 / 1 + exp(-z)) for z > 0 and exp(z) / (1 + exp(z)) for z < 0, avoiding overflow errors.
 
-    result = np.zeros_like(a=z, dtype=float)
-    #Boolean matrices contain 0s (false) or 1s (true).
-    positiveMask = z > 0  #Mask for values > 0
-    negativeMask = z <= 0 #Mask for values <= 0
+        This method creates two masks of the inputted matrix, one for values > 0 and another for values <= 0.
+        The optimized version of the sigmoid is applied to a result matrix with respect to the masks.
+        
+        (Source: https://blog.dailydoseofds.com/p/sigmoid-and-softmax-are-not-implemented)
+        """
 
-    #The masks are being used inside brackets to have the function apply only to values with true.
-    a = np.exp(-1 * z[positiveMask])
-    result[positiveMask] = 1 / (1 + a)
+        result = np.zeros_like(a=x, dtype=float)
+        #Boolean matrices contain 0s (false) or 1s (true).
+        positiveMask = x > 0  #Mask for values > 0
+        negativeMask = x <= 0 #Mask for values <= 0
 
-    a = np.exp(z[negativeMask])
-    result[negativeMask] = a / (a + 1)
-    
-    return result
+        #The masks are being used inside brackets to have the function apply only to values with true.
+        a = np.exp(-1 * x[positiveMask])
+        result[positiveMask] = 1 / (a + 1)
 
-def derivSigmoid(z: ndarray) -> ndarray:
-    a = sigmoid(z)
-    return a * (1 - a)
+        a = np.exp(x[negativeMask])
+        result[negativeMask] = a / (a + 1)
+        
+        return result
+
+    @staticmethod
+    def __deriv(x: ndarray) -> ndarray:
+        a = Sigmoid.__sigmoid(x)
+        return a * (1 - a)
 
 class ReLU(ActivationFunction):
     @staticmethod
-    def getFunc():
-        return relu
+    def getFunc(input: ndarray):
+        return ReLU.__relu(input)
     
     @staticmethod
-    def getDeriv():
-        return derivRelu
+    def getDeriv(input: ndarray):
+        return ReLU.__deriv(input)
     
-def relu(x: ndarray) -> ndarray:
-    return np.maximum(0, x)
+    @staticmethod
+    def __relu(x: ndarray) -> ndarray:
+        return np.maximum(0, x)
 
-def derivRelu(x: ndarray) -> ndarray:
-    positiveMask = x > 0
-    result = np.zeros_like(a=x, shape=x.shape)
-    result[positiveMask] = 1
+    @staticmethod
+    def __deriv(x: ndarray) -> ndarray:
+        positiveMask = x > 0
+        result = np.zeros_like(a=x, shape=x.shape)
+        result[positiveMask] = 1
 
-    return result
+        return result
+
+class LeakyReLU(ReLU):
+    """Note:: Uses a coefficient of 0.01 for negatives. Also extends ReLU."""
+
+    @staticmethod
+    def getFunc(input: ndarray):
+        return LeakyReLU.__leakyRelu(input)
+    
+    @staticmethod
+    def getDeriv(input: ndarray):
+        return LeakyReLU.__deriv(input)
+    
+    @staticmethod
+    def __leakyRelu(x: ndarray) -> ndarray:
+        negativeMask = x < 0
+        result = np.ones_like(a=x, shape=x.shape)
+
+        result *= x
+        result[negativeMask] *= 0.01
+
+        return result
+
+    @staticmethod
+    def __deriv(x: ndarray) -> ndarray:
+        negativeMask = x < 0
+        result = np.ones_like(a=x, shape=x.shape)
+
+        result[negativeMask] *= 0.01
+
+        return result
 
 #TODO
 # class GeLU:
 #     pass
 
 class Identity(ActivationFunction):
-    @staticmethod
-    def getFunc():
-        return identity
 
     @staticmethod
-    def getDeriv():
-        return derivIdentity
+    def getFunc(input: ndarray):
+        return Identity.__identity(input)
 
-def identity(x: ndarray) -> ndarray:
-    return x
+    @staticmethod
+    def getDeriv(input: ndarray):
+        return Identity.__deriv(input)
 
-def derivIdentity(x: ndarray) -> ndarray:
-    return np.ones(x.shape)
+    @staticmethod
+    def __identity(x: ndarray) -> ndarray:
+        scalar = 1.
+        return scalar * x
 
-#FIXME
-# def softmax(mat: ndarray) -> ndarray:
-#     """Takes a matrix as an argument and calculates a probability distribution 
-#     represented by a matrix with the same shape."""
+    @staticmethod
+    def __deriv(x: ndarray) -> ndarray:
+        scalar = 1.
+        return scalar * np.ones(x.shape)
 
-#     sumExp = np.sum(np.exp(mat))
-#     return np.exp(mat) / sumExp
+#TODO
+# class Softmax(ActivationFunction):
+#     pass
